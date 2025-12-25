d3fd31def86a59674b98ebab1826f25fc66ac2f1
trevcruz182 <trevorcruz182@gmail.com>
2025-12-25 00:41:55 -0500

    Update TypeCard.jsx
    Fixed typo in class name causing bug in the way types were displayed.

--- src/components/TypeCard.jsx
+++ src/components/TypeCard.jsx
@@ -4,7 +4,7 @@ import { pokemonTypeColors } from '../utils'
 export default function TypeCard(props) {
     const { type } = props
     return (
-        <div className='type-title' style={{color: pokemonTypeColors?.[type]?.color, background: pokemonTypeColors?.[type]?.background}}>
+        <div className='type-tile' style={{color: pokemonTypeColors?.[type]?.color, background: pokemonTypeColors?.[type]?.background}}>
             <p>{type}</p>
         </div>
     )
